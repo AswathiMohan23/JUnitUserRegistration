5e5f50631f53d73a270ed22ae10feee31d8d7738
AswathiMohan23 <achu999here@gmail.com>
2022-11-16 09:17:11 +0530

    [REFACTORED] : the code for name validation

--- src/test/java/com/java/TestLogic.java
+++ src/test/java/com/java/TestLogic.java
@@ -2,11 +2,13 @@ package com.java;
 
 import org.junit.Test;
 
+import java.util.Scanner;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class TestLogic {
     @Test
-    public void testFindMax(){
+    public void firstNameValidation(){
         assertEquals("Harry", UserRegistration.isValidFirstName("Harry"));
     }
 }
