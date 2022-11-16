052b99a1d39219bc7649aeaea0dd5ab5f40c144b
AswathiMohan23 <achu999here@gmail.com>
2022-11-16 15:31:56 +0530

    [ADDED] : code to check the validation of all parameters

--- src/main/java/com/java/UserRegistration.java
+++ src/main/java/com/java/UserRegistration.java
@@ -1,56 +1,77 @@
 package com.java;
 
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static String isValidName(String name) {
-        String result = nameValidation(name);
-        return result;
+    static int count = 0;
+
+    static String isEveryParametersValid(String mood) {;
+        System.out.println("count : "+count);
+        if(count==10) { // gave 10 because we are checking more than 5 emails seperately so count increments
+            mood = "Happy";
+            return mood;
+        }
+        else {
+            mood = "Sad";
+            return mood;
+        }
     }
-    private static String nameValidation(String name){
-        ArrayList<String> list=new ArrayList<String>();
-        String regex="[A-Z]{1}[a-z]{3,}";// {3,} means above 3
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(name);
-        matcher.matches();
-        if(matcher.matches()==true)
-            return  name;
-        else
+
+    static String isValidName(String name) {
+        if (nameValidation(name) == true) {
+            count++;
+            return name;
+        } else
             return null;
     }
 
-    static String isEmailValid(String email){
-        String regex="^[a-z0-9+_.-]+@+[a-z]+.+[a-z]";
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(email);
+    private static boolean nameValidation(String name) {
+        String regex = "[A-Z]{1}[a-z]{3,}";// {3,} means above 3
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
         matcher.matches();
-        if(matcher.matches()==true)
+        if (matcher.matches() == true)
+            return true;
+        else
+            return false;
+    }
+
+    static String isEmailValid(String email) {
+        String regex = "^[a-z0-9+_.-]+@+[a-z]+.+[a-z]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        matcher.matches();
+        if (matcher.matches() == true) {
+            count++;
             return email;
-        else
+        } else
             return null;
     }
+
     static String isValidPhoneNumber(String phoneNumber) {
-        String regex="[91]+() +[789][0-9]{9}";
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(phoneNumber);
+        String regex = "[91]+() +[789][0-9]{9}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
         matcher.matches();
-        if(matcher.matches()==true)
+        if (matcher.matches() == true) {
+            count++;
             return phoneNumber;
-        else
+        } else
             return null;
     }
+
     static String isValidPassword(String password) {
-         //  (?=.*?[A-Z]) ---> atleast 1 uppercase;    (?=.*?[a-z]) ----> atleast 1 lower case;
+        //  (?=.*?[A-Z]) ---> atleast 1 uppercase;    (?=.*?[a-z]) ----> atleast 1 lower case;
         //  (?=.*?[0-9]) ----> atleast 1 digit;        (?=.*?[#?!@$%^&*-]) ----> atleast 1 special character
 
-        String regex="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(password);
-        if(matcher.matches()==true)
+        String regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.matches() == true) {
+            count++;
             return password;
-        else
+        } else
             return null;
     }
 }
--- src/test/java/com/java/TestLogic.java
+++ src/test/java/com/java/TestLogic.java
@@ -8,19 +8,18 @@ import java.util.Scanner;
 import static junit.framework.TestCase.assertEquals;
 
 public class TestLogic {
-    static ArrayList<String> list=new ArrayList<String>();
-
     @Test
-    public void check_firstName_If_Valid_Return_True(){
+    public void check_firstName_If_Valid_Return_True() {
         assertEquals("Harry", UserRegistration.isValidName("Harry"));
     }
 
     @Test
-    public void check_SecondName_If_Valid_Return_True(){
+    public void check_SecondName_If_Valid_Return_True() {
         assertEquals("Potter", UserRegistration.isValidName("Potter"));
     }
+
     @Test
-    public void check_MailID_if_Valid_Return_True(){
+    public void check_MailID_if_Valid_Return_True() {
         assertEquals("abc@yahoo.com", UserRegistration.isEmailValid("abc@yahoo.com"));
         assertEquals("abc-100@yahoo.com", UserRegistration.isEmailValid("abc-100@yahoo.com"));
         assertEquals("abc111@abc.com", UserRegistration.isEmailValid("abc111@abc.com"));
@@ -31,12 +30,22 @@ public class TestLogic {
         assertEquals("abc@gmail.com.com", UserRegistration.isEmailValid("abc@gmail.com.com"));
         assertEquals("abc+100@gmail.com", UserRegistration.isEmailValid("abc+100@gmail.com"));
     }
+
     @Test
-    public void check_PhoneNumber_If_Correct_return_True(){
+    public void check_PhoneNumber_If_Correct_return_True() {
         assertEquals("91 9734567892", UserRegistration.isValidPhoneNumber("91 9734567892"));
     }
+
     @Test
-    public void check_Password_If_Correct_return_True(){
+    public void check_Password_If_Correct_return_True() {
         assertEquals("aAbcd@#1234", UserRegistration.isValidPassword("aAbcd@#1234"));
     }
+
+    @Test
+    public void check_All_The_Entries_And_IF_True_Return_Happy() {
+        assertEquals("Happy", UserRegistration.isEveryParametersValid("mood"));
+
+
+    }
 }
+
